baee7c31c0fac60dcc19fb49d44a549249b9316a
danusorn23456 <danusorn23456@gmail.com>
2023-03-10 04:02:35 +0700

    add basic global consraint

--- src/constraint/index.js
+++ src/constraint/index.js
@@ -1,11 +1,25 @@
-// Misc
+// misc
 export const OUR_GUILD = "GoDSlayeR"
 
-// Navigation
+// status
+export const SUCCESS = "success"
+export const ERROR = "error"
+export const PENDING = "pending"
+export const LOADING = "loading"
+
+// navigation
 export const PATH_HOME_PAGE = "/"
+export const PATH_PLAYER_PAGE = "/player"
+export const PATH_DEV_PAGE = "/dev"
 export const PATH_MEMBERS_PAGE = "member";
 export const PATH_ROTMG_NEWS = "https://remaster.realmofthemadgod.com/?page_id=15"
 export const PATH_DISCORD_INVITE = "https://discord.gg/reUJxHF83M"
 
-//Query
+// query
 export const QUERY_GET_GUILD = "get-guild"
+export const QUERY_GET_PLAYER = "get-player"
+
+// time
+export const TIME_FIVE_MINUTE = 300000
+export const TIME_TEN_MINUTE = 600000
+export const TIME_INFINITY = Infinity
